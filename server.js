e82d8516a8707dc65479ff9b10bd277d9d200d58
ILFforever <hammymukura@gmail.com>
2025-11-06 22:40:29 +0700

    first commit basic server connection to FIrebase

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Import route files
+const auth = require('./routes/auth');
+
+// Mount routers
+app.use('/api/v1/auth', auth);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
